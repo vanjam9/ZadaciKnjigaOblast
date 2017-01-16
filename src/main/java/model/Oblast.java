4abcf879bba0eb24c2ac93c26726c663ec5fa48a
Android Development kurs <vanja.males9@gmail.com>
2017-01-16 21:07:16 +0100

    Uradjena klasa oblast fix #5

--- src/main/java/model/Oblast.java
+++ src/main/java/model/Oblast.java
@@ -1,7 +1,50 @@
 package model;
-
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
 /**
  * Created by androiddevelopment on 16.1.17..
  */
+@DatabaseTable(tableName="Oblast")
 public class Oblast {
+
+   /* private  int id;*/
+    private String naziv;
+    private int pocetnaStrana;
+
+
+    public static final String POLJE_NAZIV = "naziv";
+    public static final String POLJE_POCETNA_STRANA = "pocetna_strana";
+
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_NAZIV, canBeNull = false)
+    private String naziv;
+    @DatabaseField(columnName = POLJE_POCETNA_STRANA, canBeNull = false)
+    private int pocetna_strana;
+
+
+
+
+    public Oblast() {
+
+    }
+
+    public Oblast(String naziv, int pocetnaStrana) {
+        this.naziv = naziv;
+        this.pocetnaStrana = pocetnaStrana;
+    }
+
+    @Override
+    public String toString() {
+        return "Oblast{" +
+
+                "naziv ='" + naziv + '\'' +
+                ",id=" + id +
+                ", pocetna_strana =" + pocetnaStrana +
+
+                '}';
+    }
 }
